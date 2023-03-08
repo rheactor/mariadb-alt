9dc9df11eb4692d5188a064eaec1e7e8984a9970
David Rodrigues <david.rodrigues@rentalhost.net>
2023-03-08 20:40:24 -0300

    feat (InitialHandshakePacket): implemented;

---
+++ src/Protocol/Packet/InitialHandshakePacket.ts
@@ -0,0 +1,70 @@
+import { Capabilities } from "@/Protocol/Enumerations";
+import { BufferConsumer } from "@/Utils/BufferConsumer";
+
+export class InitialHandshakePacket {
+  /** DBMS protocol version (eg. MariaDB 10.x === 10). */
+  public protocolVersion: number;
+
+  /** DBMS server version (eg. "5.5.5-10.11.2-MariaDB"). */
+  public serverVersion: Buffer;
+
+  /** Connection ID 32-byte. */
+  public connectionId: number;
+
+  /** Authentication seed 8-bytes (Scramble #1). */
+  public authSeed: Buffer;
+
+  /** Authentication plugin name. */
+  public authPluginName?: Buffer;
+
+  /** Length of auth_plugin_data (scramble) if greater than 0 and capabilities has CLIENT_PLUGIN_AUTH. */
+  public authPluginNameLength: number;
+
+  /** Server capabilities. */
+  public capabilities: bigint;
+
+  /** Server default collation. */
+  public defaultCollation: number;
+
+  /** Server status. */
+  public serverStatus: number;
+
+  public constructor(data: Buffer) {
+    const bufferConsumer = new BufferConsumer(data);
+
+    /** @see https://mariadb.com/kb/en/connection/#initial-handshake-packet */
+    this.protocolVersion = bufferConsumer.readInt();
+    this.serverVersion = bufferConsumer.readNullTerminatedString();
+    this.connectionId = bufferConsumer.readInt(4);
+    this.authSeed = bufferConsumer.readString(8, true);
+    this.capabilities = BigInt(bufferConsumer.readInt(2));
+    this.defaultCollation = bufferConsumer.readInt();
+    this.serverStatus = bufferConsumer.readInt(2);
+    this.capabilities += BigInt(bufferConsumer.readInt(2)) << 16n;
+    this.authPluginNameLength = bufferConsumer.readInt();
+    bufferConsumer.skip(6);
+    this.capabilities += BigInt(bufferConsumer.readInt(4)) << 32n;
+
+    if (this.hasCapability(Capabilities.SECURE_CONNECTION)) {
+      this.authSeed = Buffer.concat([
+        this.authSeed,
+        bufferConsumer.readString(
+          Math.max(12, this.authPluginNameLength - 9),
+          true
+        ),
+      ]);
+    }
+
+    if (this.hasCapability(Capabilities.PLUGIN_AUTH)) {
+      this.authPluginName = bufferConsumer.readNullTerminatedString();
+    }
+  }
+
+  public hasCapability(capability: bigint) {
+    return (this.capabilities & capability) !== 0n;
+  }
+
+  public hasServerStatus(status: number) {
+    return (this.serverStatus & status) !== 0;
+  }
+}
---
+++ tests/Protocol/Packet/InitialHandshakePacket.test.ts
@@ -0,0 +1,31 @@
+import { Capabilities, ServerStatus } from "@/Protocol/Enumerations";
+import { InitialHandshakePacket } from "@/Protocol/Packet/InitialHandshakePacket";
+import { InitialHandshakePacket as Fixture } from "@Tests/Fixtures/InitialHandshakePacket";
+
+describe("InitialHandshakePacket", () => {
+  test("read properties", () => {
+    const packet = new InitialHandshakePacket(Fixture.Example1);
+
+    expect(packet.protocolVersion).toBe(0x0a);
+    expect(packet.serverVersion).toStrictEqual(Buffer.from("example"));
+    expect(packet.connectionId).toBe(0x40302010);
+    expect(packet.authSeed).toStrictEqual(Buffer.from("ScrambleScramble1234"));
+    expect(packet.capabilities.toString(16)).toBe("1d81fff7fe");
+    expect(packet.defaultCollation).toBe(0x2d);
+    expect(packet.serverStatus).toBe(0x0002);
+    expect(packet.authPluginNameLength).toBe(0x15);
+    expect(packet.authPluginName).toStrictEqual(
+      Buffer.from("mysql_native_password")
+    );
+
+    expect(packet.hasCapability(Capabilities.CLIENT_MYSQL)).toBe(false);
+    expect(packet.hasCapability(Capabilities.SECURE_CONNECTION)).toBe(true);
+
+    expect(packet.hasServerStatus(ServerStatus.SERVER_QUERY_WAS_SLOW)).toBe(
+      false
+    );
+    expect(packet.hasServerStatus(ServerStatus.SERVER_STATUS_AUTOCOMMIT)).toBe(
+      true
+    );
+  });
+});
