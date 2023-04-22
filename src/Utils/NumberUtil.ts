aefbdedf9ea06944ac65e615dd81937b30af9435
David Rodrigues <david.rodrigues@rentalhost.net>
2023-04-22 00:51:44 -0300

    feat (NumberUtil): added toNumber() that transforms number, bigint or string to number or bigint, depending of the value requirement;

---
+++ src/Utils/NumberUtil.ts
@@ -0,0 +1,19 @@
+export const toNumber = (
+  value: bigint | number | string
+): bigint | number | undefined => {
+  if (typeof value === "number") {
+    return value;
+  }
+
+  if (typeof value === "bigint") {
+    return value >= Number.MIN_SAFE_INTEGER && value <= Number.MAX_SAFE_INTEGER
+      ? Number(value)
+      : value;
+  }
+
+  if (value.includes(".")) {
+    return Number(value);
+  }
+
+  return toNumber(BigInt(value));
+};
